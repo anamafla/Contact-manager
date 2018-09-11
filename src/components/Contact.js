8b6ea35755adb3102eaa082dfadc36d09fd42898
Ana Mafla <anamafla@hotmail.com>
2018-09-11 14:52:37 -0500

    Creating and changing State

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,26 +4,53 @@ import PropTypes from 'prop-types'
 
 
 class Contact extends Component {
-    render() {
+    
+    state = {
+        showContactInfo: false
+    };
+    
+    onShowClick = e => {
+        this.setState({ showContactInfo: !this.state.showContactInfo })
+        
+    }
 
-        const { name, email, phone } = this.props;
+    onDelectClick = () => {
+        this.props.deleteClickHandler();
+    }
+    
+    render() {
+        const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
         return  (
             <div className="card card-body mb-3">
-                <h4> {name} </h4>
+                <h4> 
+                {name}{''} 
+                <i 
+                onClick={this.onShowClick} className="fas fa-sort-down"
+                style={{ cursor: 'pointer' }}
+                />
+                <i 
+                 onClick={this.onDelectClick}className="fas fa-times"
+                 style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                />
+
+
+                </h4>
+                { showContactInfo ? (
                 <ul className="list-group">
                     <li className="list-group-item" >Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
                 </ul>
-
+                ) : null  }
             </div>
         )
     }
 }
 
 Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired,
+    contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
+    
 }
 
 
